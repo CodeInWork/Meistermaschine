02b2872b63259e7d1993df18d113d6bf0a9e9d3f
CodeInWork <fischerp@gmx.de>
2026-03-16 19:04:06 +0100

    ButtonUtils to expand to 32bit architecture (2xMCP23017=32bit)

--- Meistermaschine/lib/Buttons/ButtonUtils.h
+++ Meistermaschine/lib/Buttons/ButtonUtils.h
@@ -3,25 +3,62 @@
 
 #include <Arduino.h>
 
-inline bool singleSetBitToButtonId(uint32_t mask, uint8_t& buttonId)
+namespace ButtonLayout
 {
-    if (mask == 0) return false;
+    static constexpr uint8_t COLUMN_COUNT = 4;
+    static constexpr uint8_t ROW_COUNT = 5;
+    static constexpr uint8_t BUTTON_COUNT = COLUMN_COUNT * ROW_COUNT;
+    static constexpr uint8_t NO_BUTTON = 255;
 
-    if ((mask & (mask - 1)) != 0)
-        return false;
+    using ButtonId = uint8_t;
+    using ButtonMask = uint32_t;
 
-    buttonId = static_cast<uint8_t>(__builtin_ctzl(static_cast<unsigned long>(mask)));      // count trailing zeros
-    return true;
-}
+    constexpr ButtonId toButtonId(uint8_t column, uint8_t row) noexcept
+    {
+        return static_cast<ButtonId>(column * ROW_COUNT + row);
+    }
 
-inline uint8_t buttonColumn(uint8_t buttonId)
-{
-    return buttonId / 5;
-}
+    constexpr uint8_t columnOf(ButtonId buttonId) noexcept
+    {
+        return static_cast<uint8_t>(buttonId / ROW_COUNT);
+    }
 
-inline uint8_t buttonRow(uint8_t buttonId)
-{
-    return buttonId % 5;
+    constexpr uint8_t rowOf(ButtonId buttonId) noexcept
+    {
+        return static_cast<uint8_t>(buttonId % ROW_COUNT);
+    }
+
+    constexpr ButtonMask ID2Mask(ButtonId buttonId) noexcept
+    {
+        return static_cast<ButtonMask>(1UL << buttonId);
+    }
+
+    constexpr bool isValid(ButtonId buttonId) noexcept
+    {
+        return buttonId < BUTTON_COUNT;
+    }
+
+    inline bool singleSetBitToButtonId(ButtonMask mask, ButtonId& buttonId) noexcept
+    {
+        if (mask == 0) {
+            return false;
+        }
+
+        if ((mask & (mask - 1)) != 0) {
+            return false;
+        }
+
+        buttonId = static_cast<ButtonId>(
+            __builtin_ctzl(static_cast<unsigned long>(mask))
+        );
+
+        return isValid(buttonId);
+    }
+
+    inline ButtonMask lowestSetBit(ButtonMask mask) noexcept
+    {
+        return mask & static_cast<ButtonMask>(-static_cast<int32_t>(mask));
+    }
 }
 
 #endif
